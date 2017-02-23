e57e97ee94acb519aef8ca1221d456e1916c2202
feihongla <1185089434@qq.com>
2017-02-23 13:29:27 +0800

    Update count_words.py

--- count_words.py
+++ count_words.py
@@ -9,14 +9,14 @@
 import codecs
 from collections import Counter
 
-lodefile = codeces.open("happiness_seg.txt", "r", encoding = 'utf-8') #打开文件
+lodefile = codecs.open("happiness_seg.txt", "r", encoding = 'utf-8') #打开文件
 words_list = lodefile.read().split(" ") #把文章内容进行分割，并返回字符串列表
 
 words_dicts = Counter() #建立一个新的计数字典来存放二元词组
 
 #筛选符合定义的二元词组组成字典
 for i in range(0, (len(words_list)-1)):
-	if len(words_list[i]) >=2 len(words_list[i+1]) >=2:
+	if len(words_list[i]) >=2 and len(words_list[i+1]) >=2:
 		couple_word = words_list[i] + words_list[i+1]
 		#去掉单字词，并保持词组连续
 		words_dicts[couple_word] +=1
